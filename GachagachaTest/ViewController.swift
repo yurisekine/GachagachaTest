1eff64ceb89448e44caa5a971ed42517d7547538
SEKINE YURI <yuri.1027ex@gmail.com>
2016-02-13 10:53:55 +0900

    Initial Commit

---
+++ GachagachaTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GachagachaTest
+//
+//  Created by SEKINE YURI on 2016/02/13.
+//  Copyright © 2016年 SEKINE YURI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
